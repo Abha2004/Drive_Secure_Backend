3c60258342552ab00373e77c6ffc1f70db28cd06
Abha2004 <Abha2004@users.noreply.github.com>
2026-05-30 14:45:48 +0530

    Use bulletproof RegExp wildcard route for Express 5

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use("/api/location", locationRoutes);
 if (process.env.NODE_ENV === "production") {
   const clientBuildPath = path.join(__dirname, "..", "frontend", "dist");
   app.use(express.static(clientBuildPath));
-  app.get("/*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(clientBuildPath, "index.html"));
   });
 }
